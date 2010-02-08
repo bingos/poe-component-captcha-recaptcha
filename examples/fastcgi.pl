63d253c2194022706975d176b0b0b248389a5e87
Chris Williams <chris@bingosnet.co.uk>
2010-02-08 14:25:49 +0000

    More stuff

--- examples/fastcgi.pl
+++ examples/fastcgi.pl
@@ -1,6 +1,12 @@
 use strict;
 use warnings;
+use CGI::Simple;
 use POE qw(Component::FastCGI Component::Captcha::reCAPTCHA);
+
+# Your reCAPTCHA keys from
+#   https://admin.recaptcha.net/recaptcha/createsite/
+use constant PUBLIC_KEY       => '<public key here>';
+use constant PRIVATE_KEY      => '<private key here>';
 
 my $captcha = POE::Component::Captcha::reCAPTCHA->spawn();
 
@@ -29,8 +35,30 @@ sub _request {
   my ($kernel,$heap) = @_[KERNEL,HEAP];
   my $request = $_[ARG1]->[0];
 
+  my $error = undef;
+
   my $response = $request->make_response;
   $response->header("Content-type" => "text/html");
-  $response->content("A page");
+  my $content = <<EOT;
+<html>
+  <body>
+    <form action="" method="post">
+EOT
+
+  if ( $request->method eq 'POST' ) {
+
+  }
+
+  $content .= $captcha->get_html( PUBLIC_KEY, $error );
+  $content .= <<EOT;
+    <br/>
+    <input type="submit" value="submit" />
+    </form>
+  </body>
+</html>
+EOT
+
+  $response->content($content);
   $response->send;
+  return;
 }
