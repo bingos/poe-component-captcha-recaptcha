5f6a64c246ccaa663557fce52d4b22748fbf0c70
Chris Williams <chris@bingosnet.co.uk>
2010-02-08 15:25:08 +0000

    Updated the fastcgi example

--- examples/fastcgi.pl
+++ examples/fastcgi.pl
@@ -7,8 +7,9 @@ use POE qw(Component::FastCGI Component::Captcha::reCAPTCHA);
 #   https://admin.recaptcha.net/recaptcha/createsite/
 use constant PUBLIC_KEY       => '<public key here>';
 use constant PRIVATE_KEY      => '<private key here>';
+use constant FASTCGI_PORT     => 1027;
 
-my $captcha = POE::Component::Captcha::reCAPTCHA->spawn();
+my $captcha = POE::Component::Captcha::reCAPTCHA->spawn( alias => 'recaptcha' );
 
 POE::Session->create(
    package_states => [
@@ -22,7 +23,7 @@ sub _start {
   my ($kernel,$session) = @_[KERNEL,SESSION];
 
   POE::Component::FastCGI->new(
-    Port => 1027,
+    Port => FASTCGI_PORT,
     Handlers => [
         [ '.*' => $session->postback( '_request' ) ],
     ]
@@ -35,6 +36,21 @@ sub _request {
   my ($kernel,$heap) = @_[KERNEL,HEAP];
   my $request = $_[ARG1]->[0];
 
+  if ( $request->method eq 'POST' ) {
+     my $q = CGI::Simple->new( $request->content );
+     warn $_, "\n" for $q->param;
+     my %opts = (
+        event      => '_captcha',
+        privatekey => PRIVATE_KEY,
+        remoteip   => $request->env('REMOTE_ADDR'),
+        challenge  => $q->param( 'recaptcha_challenge_field' ),
+        response   => $q->param( 'recaptcha_response_field' ),
+        _request   => $request,
+     );
+     $kernel->post( 'recaptcha', 'check_answer', \%opts );
+     return;
+  }
+
   my $error = undef;
 
   my $response = $request->make_response;
@@ -45,10 +61,6 @@ sub _request {
     <form action="" method="post">
 EOT
 
-  if ( $request->method eq 'POST' ) {
-
-  }
-
   $content .= $captcha->get_html( PUBLIC_KEY, $error );
   $content .= <<EOT;
     <br/>
@@ -58,6 +70,31 @@ EOT
 </html>
 EOT
 
+  $response->content($content);
+  $response->send;
+  return;
+}
+
+sub _captcha {
+  my ($kernel,$reply) = @_[KERNEL,ARG0];
+  my $request = delete $reply->{_request};
+
+  my $response = $request->make_response;
+  $response->header("Content-type" => "text/html");
+  my $content = <<EOT;
+<html>
+  <body><pre>
+EOT
+  if ( $reply->{is_valid} ) {
+     $content .= "<p>That was a valid response</p>\n";
+  }
+  else {
+     $content .= "<p>Got $reply->{error}</p>\n";
+  }
+  $content .= <<EOT;
+  </pre></body>
+</html>
+EOT
   $response->content($content);
   $response->send;
   return;
